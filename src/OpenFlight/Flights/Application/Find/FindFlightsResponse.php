c26bf4f4db456588306dd7df3035d110b0d5ed9c
Enrique Torrents Cuevas <80000917+EnriqueTorrents@users.noreply.github.com>
2021-05-23 20:10:54 +0200

    Create FindFlightsResponse.php

---
+++ src/OpenFlight/Flights/Application/Find/FindFlightsResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace CodelyTv\OpenFlight\Flights\Application\Find;
+
+
+use CodelyTv\Shared\Domain\Bus\Query\Response;
+
+class FindFlightsResponse implements Response
+{
+
+    private array $flights;
+
+    public function __construct($flights)
+    {
+        $this->flights = $flights;
+    }
+
+    public function getFlights(): array
+    {
+        return $this->flights;
+    }
+
+}
