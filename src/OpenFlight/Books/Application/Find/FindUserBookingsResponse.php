1ec3cffed2acb163f332d17c85e80e70b8eea4ab
Enrique Torrents Cuevas <80000917+EnriqueTorrents@users.noreply.github.com>
2021-05-30 22:05:56 +0200

    FindUserBookingsResponse userId -> username

--- src/OpenFlight/Books/Application/Find/FindUserBookingsResponse.php
+++ src/OpenFlight/Books/Application/Find/FindUserBookingsResponse.php
@@ -8,13 +8,13 @@ use CodelyTv\Shared\Domain\Bus\Query\Response;
 
 class FindUserBookingsResponse implements Response
 {
-    public function __construct(private string $userId, private array $bookings)
+    public function __construct(private string $username, private array $bookings)
     {
     }
 
-    public function getUserId(): string
+    public function getUsername(): string
     {
-        return $this->userId;
+        return $this->username;
     }
 
     public function getBookings(): array
