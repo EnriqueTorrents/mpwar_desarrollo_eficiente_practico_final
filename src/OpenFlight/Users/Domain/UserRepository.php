e747d7773eeb12e600e8b9c26e7d72c35fd8eae3
Enrique Torrents Cuevas <80000917+EnriqueTorrents@users.noreply.github.com>
2021-05-30 22:04:41 +0200

    UserRepository findById

--- src/OpenFlight/Users/Domain/UserRepository.php
+++ src/OpenFlight/Users/Domain/UserRepository.php
@@ -3,9 +3,13 @@
 
 namespace CodelyTv\OpenFlight\Users\Domain;
 
+use CodelyTv\Shared\Domain\ValueObject\Uuid;
+
 interface UserRepository
 {
     public function Save(User $user): void;
 
     public function findByUsername(string $username): User;
+
+    public function findById(Uuid $userId): ?User;
 }
