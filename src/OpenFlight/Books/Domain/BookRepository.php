7581cd9c51e653286269063f14db2e892d9423b3
Enrique Torrents Cuevas <80000917+EnriqueTorrents@users.noreply.github.com>
2021-05-30 20:52:30 +0200

    Update BookRepository.php

--- src/OpenFlight/Books/Domain/BookRepository.php
+++ src/OpenFlight/Books/Domain/BookRepository.php
@@ -3,7 +3,11 @@
 
 namespace CodelyTv\OpenFlight\Books\Domain;
 
+use CodelyTv\Shared\Domain\ValueObject\Uuid;
+
 interface BookRepository
 {
     public function save(Book $book): void;
+
+    public function findByUserId(Uuid $userId): array;
 }
