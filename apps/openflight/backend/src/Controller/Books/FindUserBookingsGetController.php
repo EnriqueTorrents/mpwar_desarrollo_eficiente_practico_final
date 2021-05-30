e4a1d8a2d5a5d34d0c523ca83ff649cb1ed9203a
Enrique Torrents Cuevas <80000917+EnriqueTorrents@users.noreply.github.com>
2021-05-30 20:53:00 +0200

    Update FindUserBookingsGetController.php

--- apps/openflight/backend/src/Controller/Books/FindUserBookingsGetController.php
+++ apps/openflight/backend/src/Controller/Books/FindUserBookingsGetController.php
@@ -4,16 +4,38 @@
 namespace CodelyTv\Apps\OpenFlight\Backend\Controller\Books;
 
 
+use CodelyTv\OpenFlight\Books\Application\Find\FindUserBookingsQuery;
+use CodelyTv\OpenFlight\Books\Application\Find\FindUserBookingsResponse;
+use CodelyTv\OpenFlight\Books\Application\Find\FindUserBookResponse;
 use CodelyTv\Shared\Infrastructure\Symfony\ApiController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class FindUserBookingsGetController extends ApiController
 {
     public function __invoke(string $userId)
     {
-        $response = $this->ask();
+        /** @var FindUserBookingsResponse $response */
+        $response = $this->ask(new FindUserBookingsQuery($userId));
 
-        return new JsonResponse();
+        $userBookingsResponse = [];
+        /** @var FindUserBookResponse $book */
+        foreach ($response->getBookings() as $book) {
+            array_push(
+                $userBookingsResponse,
+                [
+                    'flight-id' => $book->getFlightId(),
+                ]
+            );
+        }
+
+        return new JsonResponse(
+            [
+                'user-id' => $response->getUserId(),
+                'bookings' => $userBookingsResponse,
+            ],
+            Response::HTTP_OK
+        );
     }
 
     protected function exceptions(): array
